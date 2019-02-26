f4e0b8128ff60d59a1d027c575fb62dbebfa06b3
Nevalyashka1337 <ivan.nedelyuk@yandex.ru>
2019-02-26 20:41:45 +0300

    add chai-jest-snapshot plugin to setup file

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,8 @@
 import Enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
+import chai from "chai";
+import chaiJestSnapshot from "chai-jest-snapshot";
+chai.use(chaiJestSnapshot);
 
 Enzyme.configure({ 
 	adapter: new Adapter(),
